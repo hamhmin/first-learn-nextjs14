4285f32a5e004efee4915e39a5bf8d9d62898d4d
함형민 <hamhmin@iitekeuui-MacBookPro.local>
2024-04-30 12:15:10 +0900

    commit message

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,12 +1,12 @@
 "use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+// import { useState } from "react";
 import styles from "../styles/navigation.module.css"
 
 export default function Navigation () {
     const path = usePathname();
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
     return(
         <nav className={styles.nav}>
             <ul className={styles.list}>
